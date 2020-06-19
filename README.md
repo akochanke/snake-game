f3f3d07c63937bad6217fe06914c346549f82c15
der-dre <a.kochanke+github@posteo.net>
2020-06-19 22:20:53 +0200

    game time included

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # The Ultimate Snake Machine
 
 This repository attempts two tasks:
-1. Build a simple snake game that can be played in the terminal. Simultaneously the game should accept machine input.
+1. Build a simple snake game that can be played in the terminal.
+Simultaneously the game should accept machine input.
 2. Build some kind of reinforcement learning algorithm, best would be in tf2
 
 ## Status:
--- snake_game.py
+++ snake_game.py
@@ -1,28 +1,43 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 22 18:14:06 2019
-
-@author: fumo
 
 code taken from https://github.com/korolvs/snake_nn/blob/master/snake_game.py
 find explanation here:
 https://towardsdatascience.com/today-im-going-to-talk-about-a-small-practical-example-of-using-neural-networks-training-one-to-6b2cbd6efdb3
 """
 
-# -*- coding: utf-8 -*- 
+
+# import packages
+import argparse
 import curses
 from random import randint
 import numpy as np
 
+# define classes
 class SnakeGame:
-    def __init__(self, board_width = 20, board_height = 20, gui = False):
+    def __init__(self, board_width=20, board_height=20, game_steps=50,
+                 gui = False):
+        '''Class initiation
+
+        Arguments:
+        - board_width: int, width of game field
+        - boad_height: int, height of game field
+        - game_steps: int, length of game
+        - gui: bool, if set GUI is used via curses
+
+        '''
+
         self.score = 0
         self.time = 0
+        self.game_steps = game_steps
         self.done = False
         self.board = {'width': board_width, 'height': board_height}
         self.gui = gui
+        self.end = ''
 
     def start(self):
+        '''Starting procedure of the game
+        '''
         self.snake_init()
         self.generate_food()
         if self.gui: 
@@ -30,10 +45,21 @@ class SnakeGame:
         return self.generate_observations()
 
     def snake_init(self, start_box=5):
+        '''Initialize the snake body
+
+        Arguments:
+        - start_box: int, edge width in which snake does NOT start
+
+        '''
+
+        # determine starting position
         x = randint(start_box, self.board["width"] - start_box)
         y = randint(start_box, self.board["height"] - start_box)
+
+        # create snake body
         self.snake = []
         vertical = randint(0,1) == 0
+
         for i in range(3):
             point = [x + i, y] if vertical else [x, y + i]
             self.snake.insert(0, point)
@@ -54,18 +80,34 @@ class SnakeGame:
         return key
     
     def opposite(self, key):
+        '''Function to return opposide direction of key
+
+        Arguments:
+        - key: int, direction encoded in integer
+
+        '''
+
         mapping = {105: 107, 106: 108, 107: 105, 108: 106}
         return mapping[key]
     
     def generate_food(self):
+        '''Function to place food at a random position in game field
+        '''
+
         food = []
         while food == []:
-            food = [randint(1, self.board["width"]), randint(1, self.board["height"])]
+            food = [randint(1, self.board["width"]),
+                    randint(1, self.board["height"])]
+
+            # do not place food into snake, duh!
             if food in self.snake: 
                 food = []
         self.food = food
 
     def render_init(self):
+        '''Function to initiate curses screen
+        '''
+
         curses.initscr()
         win = curses.newwin(self.board["width"] + 2, self.board["height"] + 2, 0, 0)
         curses.curs_set(0)
@@ -75,6 +117,9 @@ class SnakeGame:
         self.render()
 
     def render(self):
+        '''Render curses session
+        '''
+
         self.win.clear()
         self.win.border(0)
         self.win.addstr(0, 2, 'Score : ' + str(self.score) + ' ')
@@ -91,28 +136,53 @@ class SnakeGame:
         #self.win.getch()
 
     def step(self, key):
+        '''Function to proceed game for one step
+
+        Arguments:
+        - key: int, direction encoded as integer
+
+        '''
+
+        # check if ending condition is fulfilled
         if self.done == True: 
             self.end_game()
-        
+
         # continue direction if no key is pressed or key points backwards
-        if key == -1 or key == self.opposite(self.direction):
+        if (key == -1
+            or key == self.opposite(self.direction)
+            or key not in [105, 106, 107, 108]):
             key = self.direction
-        else: # otherwise store direction
+        # otherwise store direction of key
+        else: 
             self.direction = key
-        
+
         self.create_new_point(key)
         if self.food_eaten():
             self.score += 1
             self.generate_food()
         else:
             self.remove_last_point()
+
+        # condition check
         self.check_collisions()
+        self.check_game_steps()
+
         if self.gui: 
             self.render()
+
+        # set in-game time +1
         self.time += 1
+
         return self.generate_observations()
 
     def create_new_point(self, key):
+        '''Function to extend snake body
+
+        Arguments:
+        - key: int, direction encoded as int
+
+        '''
+
         new_point = [self.snake[0][0], self.snake[0][1]]
         if key == 105:
             new_point[0] -= 1 # UP
@@ -132,12 +202,24 @@ class SnakeGame:
         return self.snake[0] == self.food
 
     def check_collisions(self):
+        '''function to check for collisions
+        '''
+
         if (self.snake[0][0] == 0 or
             self.snake[0][0] == self.board["width"] + 1 or
             self.snake[0][1] == 0 or
             self.snake[0][1] == self.board["height"] + 1 or
             self.snake[0] in self.snake[1:-1]):
             self.done = True
+            self.end = 'Snake ran against something!'
+
+    def check_game_steps(self):
+        '''Function to check in-game time
+        '''
+
+        if self.time == self.game_steps:
+            self.done = True
+            self.end = 'Game time is up ({})!'.format(self.game_steps)
 
     def generate_observations(self):
         return self.done, self.score, self.snake, self.food
@@ -148,13 +230,27 @@ class SnakeGame:
     def end_game(self):
         if self.gui: 
             self.render_destroy()
-        print(self.snake)
-        print(self.direction)
-        raise Exception("Game over")
+        #print(self.snake)
+        #print(self.direction)
+        print(self.end)
+        exit()
+        #raise Exception(self.end)
 
 if __name__ == "__main__":
-    game = SnakeGame(gui = True)
+    # parsing command line arguments
+    parser = argparse.ArgumentParser(description= \
+        'Basic snake game. Can be played within bash via the curses package. \
+         The snake can be controlled via i (up), j (left), k (down), l \
+         (right).')
+    parser.add_argument('--game_steps', type=int, default=50,
+        help='An integer to define the length of the game.')
+
+    args = parser.parse_args()
+    GAME_STEPS = args.game_steps
+
+    # running the game
+    game = SnakeGame(game_steps=GAME_STEPS, gui=True)
     game.start()
-    for _ in range(50):
+    while True:
         input_key = game.win.getch()
         game.step(input_key)
